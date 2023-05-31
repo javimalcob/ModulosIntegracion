e0f5ede26bad5f0e776fe86d51f3f41e02a01bf9
Javier <javier.mercado.alcoba@mi.unc.edu.ar>
2023-05-31 17:45:37 -0300

    metodo de integracion de simpson compuesto

--- modulo_integracion.f90
+++ modulo_integracion.f90
@@ -3,17 +3,50 @@ use iso
 use mod_funciones
 
 implicit none
-        subroutine simpson(a, b , n, f)
+        subroutine simpson(a, b , n, f, sx)
                 !Descripcion del metodo 
                 !Regla de Simpson Compuesta de integracion numerica
                 !Declaracion de variables
                 real(wp), intent(in)        :: a , b !extremos de integracion
-                real(wp), intent(in)        :: f    !funcion f
-                integer(il) intent(in)    :: n     !numero par para particionar
+                real(wp), intent(in)        :: f     !funcion f
+                real(wp), intent(out)       :: sx    !Valor de la integral definida
+                integer(il) intent(in)      :: n     !numero par para particionar
                 
-                !Declaracion 
-          
-
+                !Declaracion de variables auxilares
+                real(wp)                :: h        !longitud del intervarlo
+                real(wp)                :: sx0      !suma acumulada de los puntos a y b
+                real(wp)                :: sx1      !suma acumulada de los puntos impares
+                real(wp)                :: sx2      !suma acumulada de los puntos pares
+                real(wp)                :: x   
+                integer(il)             :: j 
+                
+                !inicializacion de variables
+                sx0 = f(a) + f(b)
+                sx1 = 0.0_wp
+                sx2 = 0.0_wp       
+                x = a
+                
+                !Verificar que el n pasado como argumento es par
+                
+                
+                
+                !Procesamiento
+                do j = 1, n-1 
+                    x = x + h
+                    if (mod(j,2) == 0) then
+                        sx2 = sx2 + f(x)
+                    else
+                        sx1 = sx1 + f(x)
+                    end if
+                end do
+                
+                sx = (sx0 + 2._wp*sx2 + 4._wp*sx1) * h/3._wp
+                
+                
+                
+                
+                
+                
         end subroutine simpson
 
 
